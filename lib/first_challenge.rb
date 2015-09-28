eb19c22b865d8c3895d19ac25dc1ee1582e1290a
skipsuva <skip.suva@gmail.com>
2015-09-28 06:56:14 -0400

    Manipulation master

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,6 +14,16 @@ def first_challenge
   }
 
   #your code here
+  contacts["Freddy Mercury"].each do |attributes, data|
+    if attributes == :favorite_icecream_flavors
+      data.delete_if {|x| x == "strawberry"}
+    end
+  end
+
+
+
+        
+
 
 
   #remember to return your newly altered contacts hash!
