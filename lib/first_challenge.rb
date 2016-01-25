0d9854ba6e9555d9270fea09567b82c927afa6cd
Matt Cassara <cassaram09@gmail.com>
2016-01-24 23:08:27 -0500

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,10 +13,14 @@ def first_challenge
     }
   }
 
-  #your code here
+  contacts.each do |person, data|
+    data.each do |item, attribute|
+      if item == :favorite_icecream_flavors
+        attribute.delete_if {|flavor| flavor == "strawberry"}
+      end
+    end
+  end
 
-
-  #remember to return your newly altered contacts hash!
   contacts
 end
 
