62089ed82ef4721c0fea374d10884ea4a9c879e6
gs2589 <gabriel.seidner@gmail.com>
2016-02-12 16:52:00 -0500

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,8 +13,14 @@ def first_challenge
     }
   }
 
-  #your code here
-
+contacts.each do |user,data|
+  contacts.each do |attribute, value|
+    if data[:name]=="Freddy"
+      data[:favorite_icecream_flavors].delete("strawberry")
+    end
+  end
+end
+    
 
   #remember to return your newly altered contacts hash!
   contacts
