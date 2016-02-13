85a2495b8bb0ff7559ffad3f4dfe0da8d82cc7dd
tyleresmith <oats000@hotmail.com>
2016-02-13 17:48:18 +0000

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -5,9 +5,11 @@ def second_challenge
    meat: ["chicken", "steak", "salmon"],
    grains: ["rice", "pasta"]
   }
-
+  a=[]
   #code your solution here!
-
-  
+  a << groceries.values
+  a.flatten!
 
 end
+
+puts second_challenge
