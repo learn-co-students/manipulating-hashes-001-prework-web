07d9717bf3bbbb0db825615284b19c85c7c3a5cb
wengong <wentoshare@gmail.com>
2016-03-14 23:53:52 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -2,7 +2,7 @@ def first_challenge
   contacts = {
     "Jon Snow" => {
       name: "Jon",
-      email: "jon_snow@thewall.we", 
+      email: "jon_snow@thewall.we",
       favorite_icecream_flavors: ["chocolate", "vanilla", "mint chip"],
       knows: nil
     },
@@ -14,8 +14,8 @@ def first_challenge
   }
 
   #your code here
-
-
+#first_challenge["Freddy Mercury"][:favorite_icecream_flavors]).to_not include("strawberry")
+  contacts["Freddy Mercury"][:favorite_icecream_flavors].delete("strawberry")
   #remember to return your newly altered contacts hash!
   contacts
 end
