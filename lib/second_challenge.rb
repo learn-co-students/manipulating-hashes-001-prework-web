39a141ea51ced4e62410d97d67d92151d59b5e8e
Tal <talgoldfus@gmail.com>
2016-03-19 18:31:59 +0000

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -8,6 +8,18 @@ def second_challenge
 
   #code your solution here!
 
-  
+array=[]
+  groceries.values.each do |values|
+   array << values
+  end
+array.flatten
+
+
+#Option without .Value
+#  array=[]
+#  groceries.each do |key,value|
+#    array << value
+#  end
+# array.flatten
 
 end
