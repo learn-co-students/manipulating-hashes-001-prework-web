d9e3b5f3be78cccac03e0c6f5d9e38707e68adbf
Salome Braganza <salomekbg@gmail.com>
2016-06-06 21:03:43 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,7 @@ def first_challenge
   }
 
   contacts["Freddy Mercury"].each do |attribute, value|
-    if attribute == :favorite_icecream_flavors && value.include?("strawberry")
+    if attribute == :favorite_icecream_flavors
       value.delete("strawberry")
     end
   end
