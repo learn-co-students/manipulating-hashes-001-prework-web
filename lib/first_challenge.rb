66ba3256eb2743c09233365f6061a9fda39260ce
junklpm <lance.p.miller@gmail.com>
2016-09-05 20:34:25 -0400

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,7 +13,21 @@ def first_challenge
     }
   }
 
-  #your code here
+contacts["Freddy Mercury"].each do |attribute, value|
+  if attribute == :favorite_icecream_flavors
+    value.delete_if do |flavor|
+      flavor == "strawberry"
+    end
+  end
+end
+
+
+#line 1: Of all the contacts we want Freddy Mercury
+#iterate through each attribute and value in the Freddy Mercury Hash
+#line 2: if one of the attributes equals a key of favorite ice cream flavors
+#line 3: delete the flavor strawberry. Since favorite ice cream flavors is an array
+#iterate through the array, and delete if the flavor is strawberry.
+
 
 
   #remember to return your newly altered contacts hash!
